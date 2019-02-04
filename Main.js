9a603d49f6daccf0ae4f231418002520f96d6c53
Antoni Dalmau <38256452+AntoniDalmauDieguez@users.noreply.github.com>
2019-02-04 19:56:59 +0100

    Create Main.js

---
+++ Main.js
@@ -0,0 +1,74 @@
+import express from 'express'
+import request from 'request'
+
+import { getCommand } from './structCommands'
+import { getBoss } from './structBosses'
+import { getDayTimetable } from './structBossTimetable'
+import { getSurvey } from './structSurveys'
+
+const { PORT, DISCORD_API_TOKEN, PROJECT_DOMAIN } = process.env
+
+// Bot
+var Discord = require("discord.js");
+var bot = new Discord.Client();
+var d = new Date()
+
+const day = function(day, msg) {
+  //msg.channel.send((d.getHours()+1+d.getMinutes()/100))
+  //msg.channel.send(getDayTimetable('horas').hora1)
+  //msg.channel.send((d.getHours()+1) < getDayTimetable('horas').hora1)
+  var x = " will spawn at: "
+  if((d.getHours()+1+d.getMinutes()/100) < getDayTimetable('horas').hora0) x = getDayTimetable('dia'+day).hora0+x+getDayTimetable('horas').hora0+'h'
+  else if((d.getHours()+1+d.getMinutes()/100) < getDayTimetable('horas').hora1) x = getDayTimetable('dia'+day).hora1+x+getDayTimetable('horas').hora1+'h'
+  else if((d.getHours()+1+d.getMinutes()/100) < getDayTimetable('horas').hora2) x = getDayTimetable('dia'+day).hora2+x+getDayTimetable('horas').hora2+'h'
+  else if((d.getHours()+1+d.getMinutes()/100) < getDayTimetable('horas').hora3) x = getDayTimetable('dia'+day).hora3+x+getDayTimetable('horas').hora3+'h'
+  else if((d.getHours()+1+d.getMinutes()/100) < getDayTimetable('horas').hora4) x = getDayTimetable('dia'+day).hora4+x+getDayTimetable('horas').hora4+'h'
+  else if((d.getHours()+1+d.getMinutes()/100) < getDayTimetable('horas').hora5) x = getDayTimetable('dia'+day).hora5+x+getDayTimetable('horas').hora5+'h'
+  else if((d.getHours()+1+d.getMinutes()/100) < getDayTimetable('horas').hora6) x = getDayTimetable('dia'+day).hora6+x+getDayTimetable('horas').hora6+'h'
+  else if((d.getHours()+1+d.getMinutes()/100) < getDayTimetable('horas').hora7) x = getDayTimetable('dia'+day).hora7+x+getDayTimetable('horas').hora7+'h'
+  else if((day+1) === 6) x = getDayTimetable('dia0').hora0+x+getDayTimetable('horas').hora0+'h'
+  else x = getDayTimetable('dia'+(day+1)).hora0+x+getDayTimetable('horas').hora0+'h'
+  msg.channel.send(x)
+}
+
+// This code will run once the bot receives any message.
+bot.on("message", function (msg) {
+  if(msg.content === '!comandos' || msg.content === '!commands') msg.channel.send(getCommand('comandos').content)
+  else if(msg.content === '!cooking' || msg.content === '!cocina') msg.channel.send(getCommand('cooking').content)
+  else if(msg.content === '!trade' || msg.content === '!tradeo') msg.channel.send(getCommand('trade').content)
+  else if(msg.content === '!alchemy' || msg.content === '!alquimia') msg.channel.send(getCommand('alchemy').content)
+  else if(msg.content === '!necklace' || msg.content === '!collar') msg.channel.send(getCommand('necklace').content)
+  else if(msg.content === '!epheria') msg.channel.send(getCommand('epheria').content)
+  else if(msg.content === '!gramverre' || msg.content === '!caballo') msg.channel.send(getCommand('gramverre').content)
+  else if(msg.content === '!wagon' || msg.content === '!carro') msg.channel.send(getCommand('wagon').content)
+  else if(msg.content === '!afk') msg.channel.send(getCommand('afk').content)
+  else if(msg.content === '!normas') msg.channel.send(getCommand('normas').content)
+  else if(msg.content === '!encuesta') {
+    msg.channel.send(getSurvey('bossSurvey').content).then(function(msg) {
+      msg.react("👎")
+      msg.react("👍")
+      msg.pin()
+    });
+  }
+  else if(msg.content === '!boss') day(d.getDay(), msg)
+});
+bot.login(DISCORD_API_TOKEN)
+
+// Web server
+const webServer = express()
+const keepalive = () => 
+  PROJECT_DOMAIN &&
+  request(
+    { url: `https://${PROJECT_DOMAIN}.glitch.me/glitch-alive` },
+    () => setTimeout(keepalive, 55000),
+  )
+
+webServer.use('/', express.static('public'))
+
+if (PROJECT_DOMAIN)
+  webServer.get('/glitch-alive', (req, res) => res.send(`I'm alive`))
+
+webServer.listen(
+  PORT,
+  () => console.log(`[Express]-Listening on port ${PORT}!`) || keepalive(),
+)
